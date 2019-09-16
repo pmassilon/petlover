59d6ccb23f8bbf92d9a5434f9d41a6ea27bae9c8
massilon <p.massilon@gmail.com>
2019-09-15 22:41:39 -0300

    (+) Views
    - application
    - ApplicationHelper
    - flash messages
    - form erros messages
    - Navbar
    - Pets
    -- Index
    -- New
    -- Edit
    
    - Devise
    -- Log in
    -- Sing Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+  def bootstrap_alert_class_for(kind)
+    klass = bootstrap_class_for(kind)
+    prefix ='alert'
+    bootstrap_make_class_for(prefix, kind)
+  end
+
+  def bootstrap_text_class_for(kind)
+    klass = bootstrap_class_for(kind)
+    prefix ='text'
+
+    bootstrap_make_class_for(prefix, kind)
+  end
+
+  def bootstrap_make_class_for(prefix, kind)
+    klass = bootstrap_class_for(kind)
+
+    "#{prefix}-#{klass}"
+  end
+
+  private
+
+  def bootstrap_class_for(kind)
+    case kind
+    when 'success'
+      'success'
+    when 'error'
+      'danger'
+    when 'alert'
+      'warning'
+    when 'notice'
+      'info'
+    else
+      return kind.to_s
+    end
+  end
 end
